77ddb1727461a1fc064bb019afc3909683f78027
yutailang0119 <yutailang0119@gmail.com>
2021-12-13 17:43:33 +0900

    Add project

---
+++ Noisier/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Noisier
+//
+//  Created by Yutaro Muta on 2021/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Noisier/NoisierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoisierApp.swift
+//  Noisier
+//
+//  Created by Yutaro Muta on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct NoisierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
